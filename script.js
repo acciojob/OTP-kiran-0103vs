6774bef2f70b01769a160cee7f19b852a5b58eb0
kiran-0103vs <131228987+kiran-0103vs@users.noreply.github.com>
2024-08-21 21:55:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', () => {
+    const inputs = document.querySelectorAll('.code');
+
+    inputs.forEach((input, index) => {
+        input.addEventListener('input', () => {
+            if (input.value.length === 1 && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        });
+
+        input.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace') {
+                if (input.value.length === 0 && index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }
+        });
+    });
+});
